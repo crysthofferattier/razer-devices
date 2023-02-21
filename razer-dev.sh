727185f3633624c7de7b899b0e5c18b2a005cfa9
crysthofferattier <crysthofferattier@gmail.com>
2023-02-21 22:43:58 +0000

    Mouse low battery level notificaton

--- razer-dev.sh
+++ razer-dev.sh
@@ -1,14 +1,16 @@
 #/bin/bash
 
+
+# Setting config to make notify-send work with cronjob
 userid=$(id -u)
 DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$userid/bus"
 export DBUS_SESSION_BUS_ADDRESS
 
+# Getting battery level
 BATTERY_LEVEL=$(polychromatic-cli -s PM2124H12700886 -k | grep "Battery" | cut -d'y' -f 2 | cut -d'%' -f 1)
 
-echo $BATTERY_LEVEL
-
-if [ $BATTERY_LEVEL -gt 20 ]
+# If battery level is lower than 20% show notification
+if [ $BATTERY_LEVEL -lt 20 ]
 then
     notify-send --urgency=critical "Low battery!!! $BATTERY_LEVEL%" \
     "Please charge Razer Viper Ultimate (Wireless)"
